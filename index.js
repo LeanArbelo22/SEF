0d326c7bf6e2c1a37215a25af4b00a20fc036c8e
Leandro <lean.arbelo.dev@gmail.com>
2022-09-28 13:45:37 -0300

    status

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ const app= express();
 const httpServer = createServer(app);
 const io = new  Server(httpServer, {
   cors:{
- origin:'https://sef-frontend-production.up.railway.app'
+ origin:'https://sef-frontend-production.up.railway.app', 
+ 'https://sef-production-a2d4.up.railway.app'
 }
 });
 
