437c4082ba15c2c6dad11d024b24f211a10529b2
LeanArbelo22 <leo.arbelo22@gmail.com>
2022-10-10 15:25:55 -0300

    intentando refactorizar index

--- db/services/seller.services.js
+++ db/services/seller.services.js
@@ -1,35 +1,35 @@
 const { now } = require('moment');
-const {models} = require('./../../libs/sequelize');
+const { models } = require('./../../libs/sequelize');
 
 
 
-async function createSeller(client, myCustomId){
+async function createSeller(client, myCustomId) {
   let clientInfo = await client.info;
-   let numberNew =   await clientInfo.wid.user;
-     let nameNew = myCustomId;
-    
-let vID = await nameNew+'_'+numberNew;
-   let [Vendedor, created] = await models.Vendedor.findOrCreate({
-    where: { number:numberNew  },
+  let numberNew = await clientInfo.wid.user;
+  let nameNew = myCustomId;
+
+  let vID = await nameNew + '_' + numberNew;
+  let [Vendedor, created] = await models.Vendedor.findOrCreate({
+    where: { number: numberNew },
     defaults: {
-      id:vID,
+      id: vID,
       number: numberNew,
       name: nameNew
-      
+
     }
   });
-if(created == true){
-console.log(numberNew + 'vendedor creado')
+  if (created == true) {
+    console.log(numberNew + 'vendedor creado')
+  }
+  return;
 }
-   return ;
-   }
 
 
 
 
 
-   async function updateSeller (client, myCustomId){
-          
+async function updateSeller(client, myCustomId) {
 
-   }
-   module.exports = {createSeller }
+
+}
+module.exports = { createSeller }
--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-// librerias
 require('dotenv').config();
 const fs = require('fs');
 const qrcode = require("qrcode-terminal");
-// funciones y variables
-/* codigo comentado 1 */
-const { saveMedia, addChat} = require('./controllers/save');
-const {models} = require('./libs/sequelize');
-const {createSeller} = require('./db/services/seller.services.js');
-const {updateCLients} = require('./db/services/client.services.js');
-const {GETMessages} = require('./db/services/messages.services.js');
-const  routerApi = require('./routes');
-// const qr = require('qr-image');
+const { addChat } = require('./controllers/save');
+const { models } = require('./libs/sequelize');
+const { createSeller } = require('./db/services/seller.services.js');
+const routerApi = require('./routes');
 const express = require('express');
 const cors = require('cors');
 const { Client, LocalAuth } = require("whatsapp-web.js");
-const {Server} = require('socket.io');
-const {createServer} =  require('http')
-const PORT =  process.env.PORT || 3002;
+const { Server } = require('socket.io');
+const { createServer } = require('http');
+const PORT = process.env.PORT || 3002;
 
 const app = express();
 const httpServer = createServer(app);
+
 const io = new Server(httpServer, {
-  cors:{
-    origin: ['https://sef-production-a2d4.up.railway.app', 'http://localhost:3000']
-  }
+    cors: {
+        origin: ['https://sef-production-a2d4.up.railway.app', 'http://localhost:3000']
+    }
 });
 
 app.use(cors());
@@ -31,234 +26,156 @@ routerApi(app);
 app.use(express.json());
 
 httpServer.listen(PORT, () => {
-  console.log('Port listo:', PORT)
+    console.log('Port listo:', PORT)
 });
 
-/* codigo comentado 2 */
-
 io.on("connection", socket => {
-  console.log("Socket conectado");
+    console.log("Socket conectado");
 
-  socket.on("newSeller", (newSellerName) => {
-    console.log("New seller:", newSellerName);
-    withOutSession(newSellerName); // !!
-  })
-})
-
-/* codigo comentado 3 */
-
-const withOutSession = (sellerName) => { // ?? async sin await     
-    console.log('No tenemos session guardada');
-    try {
-
-    
-      const client = new Client({
-          restartOnAuthFail: true,
-          puppeteer: {
-
-            headless: true,
-            args: [
-              '--no-sandbox',
-              '--disable-setuid-sandbox',
-              '--disable-dev-shm-usage',
-              '--disable-accelerated-2d-canvas',
-              '--no-first-run',
-              '--no-zygote',
-              '--disable-gpu'
-            ],
-          }, // * timeout
-          authStrategy: new LocalAuth({ clientId: sellerName, dataPath: './sessions'}),
-      })
-
-    client.initialize().then(() => console.log('Sesion iniciada')).catch((e) => { console.error(e) });;
-    
-    client.on('qr', qr => { 
-      try {
-        qrcode.generate(qr, { small: true });
-        console.log(qr);
-        io.emit("qrNew", qr);
-      } catch (e) {
-          console.log(e);
-          io.emit("qrError", e);
-      }
-    });
-
-    client.on('authenticated', () => console.log('Vendedor autenticado'));
-
-    // ?? estos nunca pasan    
-    // client.on('auth_failure', () => console.log('Fallo en autenticacion'))
-    // client.on('disconnected', () => console.log('Desconectado'))
-
-    client.on('ready', async () => {
-      try {
-        // connectionReady();
-        await createSeller(client, sellerName);
-        // await updateCLients(client)
-        // await GETMessages(client);
-        io.emit("okSeller");
-        console.log('Sesion lista para usarse');
-      } catch (e) {
-          console.log(e);
-          io.emit("sellerError", e);
-          delSession(sellerName);
-          client.destroy();
+    socket.on("newSeller", (newSellerName) => {
+        console.log("Evento de socket - newSeller:", newSellerName);
+        const actualSellers = getSellers();
+        if (!actualSellers.includes(newSellerName)) {
+            const newSession = newSeller(newSellerName);
+            generateSession(newSession, newSellerName, 'NUEVO');
+        } else {
+            authorizeSeller(newSellerName);
         }
-    });
-
-    client.on('disconnected', () => {
-      console.log('Desconectado, intentando reiniciar sesion');
-      client.destroy();
-      client.initialize();
     })
-    
-    client.on('message_create', async (msg) => {
-      let clientInfo = client.info;
-      let sellerNum = clientInfo.wid.user;
-  
-      let chat = await msg.getChat();
-      let add = await addChat(chat, sellerNum); // ?? let add
-  
-      var clienteNumberN = '';
-      let body =  msg.body;
-      let to = msg.to;
-      let from = msg.from;
-      let date1 = msg.timestamp;
-      let date =   date1 * 1000;
-      let fromMe =  msg.fromMe;
-      let idv =  msg.id.id;
+});
 
-      //  Este bug evitar que se publiquen estados
-      if (from === 'status@broadcast' || to === 'status@broadcast') {
-        console.log('Estado de Whatsapp');
-        return;
-      }
 
-      if (fromMe == true) {
-         clienteNumberN = msg.to; 
-      } else {
-         clienteNumberN = msg.from;
-      };
-  
-      let cID = clienteNumberN + '_' + sellerNum;
-      let idN = idv + ' by ' + cID;
+const getSellers = async () => {
+    const sellers = await models.Vendedor.find();
+    return [sellers];
+}
 
-      let m =  models.Mensaje.create({ 
-         body:body,
-         to:to,
-         from:from,
-         date:date,
-         fromMe:fromMe,
-         id:idN,
-         clienteId:cID
-      });
+const newSeller = (sellerName) => {
+    try {
+        const client = new Client({
+            restartOnAuthFail: true,
+            puppeteer: {
+                headless: true,
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--disable-accelerated-2d-canvas',
+                    '--no-first-run',
+                    '--no-zygote',
+                    '--disable-gpu'
+                ],
+            }, 
+            // ? timeout
+            // takeoverOnConflict: true,
+            // takeoverTimeoutMs: 10,
+            authStrategy: new LocalAuth({ clientId: sellerName, dataPath: './sessions' })
+        });
 
-      io.emit("newMessage", msg)
-    })
+        return client;
 
     } catch (e) {
-      console.log(e)
+        console.log(e);
     }
 }
 
-/* ********** */
-// codigo wemerson
-const auth = async (myCustomId) => {
-    const authStrategy = new LocalAuth({
-        clientId: myCustomId,
-        dataPath: './sessions' 
-    });
+const generateSession = (seller, sellerName, from) => {
+    seller.initialize()
+        .then(() => console.log('Sesion iniciada:', from))
+        .catch((e) => console.error(e));
 
-    const worker = `${__dirname}/sessions/session-${myCustomId}/Default/Service Worker`;
-
-    if (fs.existsSync(worker)) {
-      fs.rmdirSync(worker, { recursive: true });
-    }
-
-    const client = new Client({
-        restartOnAuthFail: true,
-        puppeteer: { 
-            headless: true,
-            args: [
-              '--no-sandbox',
-              '--disable-setuid-sandbox',
-              '--disable-dev-shm-usage',
-              '--disable-accelerated-2d-canvas',
-              '--no-first-run',
-              '--no-zygote',
-              '--disable-gpu'
-            ],
-        },
-      takeoverOnConflict: true,
-      takeoverTimeoutMs: 10,
-      authStrategy
-    });
-
-    client.initialize().then(() => { console.log('Sesion iniciada') }).catch((e) => { console.error(e) });
-
-    client.on('message_create', async (msg) => {
-      let clientInfo = client.info;
-      let vendedorNumber = clientInfo.wid.user;
-
-      let chat = await msg.getChat();
-      let add = await addChat(chat, vendedorNumber);
-
-      var clienteNumberN = '';
-      let body = msg.body;
-      let to = msg.to;
-      let from = msg.from;
-      let date1 = msg.timestamp;
-      let date = date1 * 1000;
-      let fromMe = msg.fromMe;
-      let idv = msg.id.id;
-
-      // Este bug evitar que se publiquen estados
-      if (from === 'status@broadcast' || to === 'status@broadcast') {
-        console.log('Estado de Whatsapp');
-        return;
-      }
-
-      if (fromMe == true) {
-        clienteNumberN = msg.to; 
-      } else{
-        clienteNumberN = msg.from;
-      };
-
-      let cID = clienteNumberN + '_' + vendedorNumber;
-      let idN = idv + ' by ' + cID;
-
-      let m =  models.Mensaje.create({ 
-        body:body,
-        to:to,
-        from:from,
-        date:date,
-        fromMe:fromMe,
-        id:idN,
-        clienteId:cID
-      });
-
-      io.emit("newMessage", msg);
-    });
-
-    client.on('ready', async () => {
-      try {
-        // connectionReady();
-        await createSeller(client, myCustomId);
-        // await updateCLients(client)
-        // await GETMessages(client);
-        io.emit("okSeller");
-        console.log('Sesion lista para usarse');
-      } catch (e) {
-          console.log(e);
-          io.emit("sellerError", e);
-          delSession(id);
-          client.destroy();
+    seller.on('qr', qr => {
+        try {
+            qrcode.generate(qr, { small: true });
+            io.emit("qrNew", qr);
+        } catch (e) {
+            console.log(e);
+            io.emit("qrError", e);
         }
     });
+
+    seller.on('authenticated', () => console.log('Vendedor autenticado'));
+
+    seller.on('auth_failure', () => console.log('Fallo en autenticacion'));
+
+    seller.on('ready', async () => {
+        try {
+            await createSeller(seller, sellerName);
+            io.emit("okSeller");
+            console.log('Sesion de vendedor creada correctamente');
+        } catch (e) {
+            seller.destroy();
+            io.emit("sellerError", e);
+        }
+    });
+
+    seller.on('disconnected', () => {
+        console.log('Desconectado, se borrara e intentara reiniciar la sesion');
+        seller.destroy();
+        seller.initialize();
+    });
+
+    seller.on('message_create', (msg) => {
+        newMessage(msg, seller)
+    });
+
 }
 
-const delSession = (id) => {
-  const sessionDir = `${__dirname}/sessions/session-${id}`;
-  if (fs.existsSync(sessionDir)) {
-    fs.rmdirSync(sessionDir, { recursive: true })
-  }
+const newMessage = async (msg, seller) => {
+    let sellerInfo = seller.info;
+    let sellerNum = sellerInfo.wid.user;
+
+    let chat = await msg.getChat();
+    await addChat(chat, sellerNum);
+
+    let whatsappNumber;
+    let body = msg.body;
+    let to = msg.to;
+    let from = msg.from;
+    let date = date1 * 1000;
+    let date1 = msg.timestamp;
+    let fromSeller = msg.fromMe;
+    let whatsappMsgID = msg.id.id; // ?
+
+    if (from === 'status@broadcast' || to === 'status@broadcast') {
+        console.log('Estado de Whatsapp');
+        return;
+    }
+
+    if (fromSeller) {
+        whatsappNumber = msg.to;
+    } else {
+        whatsappNumber = msg.from;
+    };
+
+    let clientID = whatsappNumber + '_' + sellerNum; // ?
+    let messageID = whatsappMsgID + ' by ' + clientID; // ?
+
+    await models.Mensaje.create({   // ? let message 
+        body: body,
+        to: to,
+        from: from,
+        date: date,
+        fromMe: fromSeller,
+        id: messageID,
+        clienteId: clientID
+    });
+
+    io.emit("newMessage", msg)
+}
+
+const authorizeSeller = async (sellerSession) => {
+    const worker = `${__dirname}/sessions/session-${sellerSession}/Default/Service Worker`;
+
+    if (fs.existsSync(worker)) {
+        fs.rmdirSync(worker, { recursive: true });
+    }
+
+    try {
+        const existentSeller = newSeller(sellerSession);
+        generateSession(existentSeller, sellerSession, 'EXISTENTE');
+
+    } catch (e) {
+        console.log(e);
+    }
 }
