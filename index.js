23e103ad14bb7377d962457477395d2d74cf48d5
LeanArbelo22 <leo.arbelo22@gmail.com>
2022-10-10 17:00:36 -0300

    no funciono comprobacion

--- index.js
+++ index.js
@@ -29,34 +29,18 @@ httpServer.listen(PORT, () => {
     console.log('Port listo:', PORT)
 });
 
-
-const getSellersNames = async () => {
-    const sellersList = await models.Vendedor.findAll({});
-    const sellers = sellersList.map(seller => seller.dataValues.name);
-    return sellers
-}
-
 io.on("connection", socket => {
     console.log("Socket conectado");
 
     socket.on("newSeller", async (newSellerName) => {
         console.log("Evento de socket - newSeller:", newSellerName);
 
-        const sellersArray = [];
-        await getSellersNames().then(data => sellersArray.push(data));
-        let regex = new RegExp(newSellerName, 'ig')
-        
-            if (sellersArray.includes(regex)) {
-                authorizeSeller(newSellerName);
-            } else {
-                const newSession = newSeller(newSellerName);
-                generateSession(newSession, newSellerName, 'NUEVO');
-            }
-        
+        const session = newSession(newSellerName);
+        generateSession(session, newSellerName, 'NUEVO');
     })
 });
 
-const newSeller = (sellerName) => {
+const newSession = (sellerName) => {
     try {
         const client = new Client({
             restartOnAuthFail: true,
@@ -71,7 +55,7 @@ const newSeller = (sellerName) => {
                     '--no-zygote',
                     '--disable-gpu'
                 ],
-            }, 
+            },
             // ? timeout
             // takeoverOnConflict: true,
             // takeoverTimeoutMs: 10,
@@ -100,7 +84,7 @@ const generateSession = (seller, sellerName, from) => {
         }
     });
 
-    seller.on('authenticated', () => console.log('Vendedor autenticado'));
+    seller.on('authenticated', () => console.log('QR autenticado'));
 
     seller.on('auth_failure', () => console.log('Fallo en autenticacion'));
 
@@ -121,54 +105,56 @@ const generateSession = (seller, sellerName, from) => {
         seller.initialize();
     });
 
-    seller.on('message_create', (msg) => {
-        newMessage(msg, seller)
-    });
+    seller.on('message_create', async (msg) => {
+        try {
+            let sellerInfo = seller.info;
+            let sellerNum = sellerInfo.wid.user;
 
+            let chat = await msg.getChat();
+            await addChat(chat, sellerNum);
+
+            let whatsappNumber;
+            let body = msg.body;
+            let to = msg.to;
+            let from = msg.from;
+            let date = date1 * 1000;
+            let date1 = msg.timestamp;
+            let fromSeller = msg.fromMe;
+            let whatsappMsgID = msg.id.id; // ?
+
+            if (from === 'status@broadcast' || to === 'status@broadcast') {
+                console.log('Estado de Whatsapp');
+                return;
+            }
+
+            if (fromSeller) {
+                whatsappNumber = msg.to;
+            } else {
+                whatsappNumber = msg.from;
+            };
+
+            let clientID = whatsappNumber + '_' + sellerNum; // ?
+            let messageID = whatsappMsgID + ' by ' + clientID; // ?
+
+            await models.Mensaje.create({   // ? let message 
+                body: body,
+                to: to,
+                from: from,
+                date: date,
+                fromMe: fromSeller,
+                id: messageID,
+                clienteId: clientID
+            });
+
+            io.emit("newMessage", msg)
+
+        } catch (e) {
+            console.log(e)
+        }
+    });
 }
 
-const newMessage = async (msg, seller) => {
-    let sellerInfo = seller.info;
-    let sellerNum = sellerInfo.wid.user;
-
-    let chat = await msg.getChat();
-    await addChat(chat, sellerNum);
-
-    let whatsappNumber;
-    let body = msg.body;
-    let to = msg.to;
-    let from = msg.from;
-    let date = date1 * 1000;
-    let date1 = msg.timestamp;
-    let fromSeller = msg.fromMe;
-    let whatsappMsgID = msg.id.id; // ?
-
-    if (from === 'status@broadcast' || to === 'status@broadcast') {
-        console.log('Estado de Whatsapp');
-        return;
-    }
-
-    if (fromSeller) {
-        whatsappNumber = msg.to;
-    } else {
-        whatsappNumber = msg.from;
-    };
-
-    let clientID = whatsappNumber + '_' + sellerNum; // ?
-    let messageID = whatsappMsgID + ' by ' + clientID; // ?
-
-    await models.Mensaje.create({   // ? let message 
-        body: body,
-        to: to,
-        from: from,
-        date: date,
-        fromMe: fromSeller,
-        id: messageID,
-        clienteId: clientID
-    });
-
-    io.emit("newMessage", msg)
-}
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 const authorizeSeller = async (sellerSession) => {
     const worker = `${__dirname}/sessions/session-${sellerSession}/Default/Service Worker`;
@@ -178,10 +164,17 @@ const authorizeSeller = async (sellerSession) => {
     }
 
     try {
-        const existentSeller = newSeller(sellerSession);
+        const existentSeller = newSession(sellerSession);
         generateSession(existentSeller, sellerSession, 'EXISTENTE');
 
     } catch (e) {
         console.log(e);
     }
+}
+
+
+const getSellersNames = async () => {
+    const sellersList = await models.Vendedor.findAll({});
+    const sellers = sellersList.map(seller => seller.dataValues.name);
+    return sellers
 }
